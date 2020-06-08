893c5d999e2196716223cc41dd047341a1239719
Lucien Najev <luciennajev@hotmail.com>
2020-06-08 09:32:14 +0100

    add follows migration

---
+++ db/migrate/20200608082349_create_follows.rb
@@ -0,0 +1,14 @@
+class CreateFollows < ActiveRecord::Migration[6.0]
+  def change
+    create_table :follows do |t|
+      t.integer :followed_id, null: false
+      t.integer :follower_id, null: false
+
+      t.timestamps
+    end
+
+    add_index :follows, :followed_id
+    add_index :follows, :follower_id
+    add_index :follows, [:followed_id, :follower_id], unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_06_175925) do
+ActiveRecord::Schema.define(version: 2020_06_08_082349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,16 @@ ActiveRecord::Schema.define(version: 2020_06_06_175925) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "followed_id", null: false
+    t.integer "follower_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id", "follower_id"], name: "index_follows_on_followed_id_and_follower_id", unique: true
+    t.index ["followed_id"], name: "index_follows_on_followed_id"
+    t.index ["follower_id"], name: "index_follows_on_follower_id"
   end
 
   create_table "likes", force: :cascade do |t|
